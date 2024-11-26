8498a4097cdffd716e7b93271b840033b2f819da
Ian <iancunha20@gmail.com>
2024-11-26 18:17:04 -0300

    Tela App

--- src/pages/Home/AppSection/AppSection.styles.ts
+++ src/pages/Home/AppSection/AppSection.styles.ts
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
   padding-top: 20px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   height: auto;
   margin: 0;
   background-color: black;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const Content = styled.div`
@@ -26,15 +30,56 @@ export const Content = styled.div`
   }
 `;
 
-export const Title = styled.h1`
+export const Title = styled.h2`
+  font-size: 2.3em;
+  color: white;
+  font-weight: 400;
+  @media (max-width: 768px) {
+      font-size: 1.5em;
+      text-align: center;
+  }
 `;
 
 export const SubTitle = styled.h3`
+  font-size: 1em;
+  color: white;
+  font-weight: 100;
+  @media (max-width: 768px) {
+      font-size: 0.8em;
+      text-align: center;
+  }
 `;
 
 export const Paragraph = styled.p`
 `;
 
 export const Image = styled.img`
-    width: 50px;
+    width: 700px;
+    @media (max-width: 768px) {
+      width: 100%;
+  }
+`;
+
+export const Icon = styled.img`
+    width: 100px;
+    margin-bottom: 24px;
+`;
+
+export const IconStore = styled.img`
+    width: 180px;
+    margin-bottom: 24px;
+    @media (max-width: 768px) {
+      width: 150px;
+  }
+`;
+
+export const BtnStore = styled.div`
+  margin-top: 24px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 100%;
+`;
+
+export const LinkApp = styled.a`
 `;
--- src/pages/Home/AppSection/AppSection.tsx
+++ src/pages/Home/AppSection/AppSection.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Container, Title, Paragraph, Image, Content } from './AppSection.styles';
-import appImage from '../../../assets/ai.svg';
+import { Container, Title, SubTitle, Image, Content, Icon, IconStore, BtnStore, LinkApp } from './AppSection.styles';
+import appImage from '../../../assets/App.svg';
+import icon from '../../../assets/icon.svg';
+import googlePlay from '../../../assets/gPlay.svg';
+import appStore from '../../../assets/appStore.svg';
 
 const AppSection: React.FC = () => (
   <Container>
     <Image src={appImage} alt="Aplicativo RE.AI.s" />
     <Content>
+      <Icon src={icon} alt="Icone do RE.AI.s" />
       <Title>Aplicativo para Corretores, Imobiliárias e Construtoras</Title>
-      <Paragraph>
+      <SubTitle>
         Baixe o aplicativo RE.AI.s e tenha todas as informações dos seus imóveis na palma da mão.
-      </Paragraph>
+      </SubTitle>
+      <BtnStore>
+        <LinkApp href="https://play.google.com/store/apps/details?id=br.com.youdigital.smartimobiliario" target='_blank'>
+          <IconStore src={googlePlay} alt="Icone Google Play" />
+        </LinkApp>
+        <LinkApp href="https://apps.apple.com/br/app/re-ai-s-smart-imobili%C3%A1rio/id1128234098" target='_blank'>
+          <IconStore src={appStore} alt="Icone App Store" />
+        </LinkApp>
+      </BtnStore>
     </Content>
   </Container>
 );
